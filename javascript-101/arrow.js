45c41f3360e438c602686187400bea3c46eb9619
Raswanth8 <raswanth8901@gmail.com>
2022-01-14 18:28:50 +0530

    Arrow Func

--- javascript-101/arrow.js
+++ javascript-101/arrow.js
@@ -24,3 +24,8 @@ user.printFunc()
 // Arrow: Kendall
 // This happens because this keywords gets redifined in the normal function, but not 
 // in arrow function.
+
+// The call, apply and bind methods are NOT suitable for Arrow functions -- as 
+// they were designed to allow methods to execute within different scopes -- because Arrow functions establish "this" 
+// based on the scope the Arrow function is defined within.
+
